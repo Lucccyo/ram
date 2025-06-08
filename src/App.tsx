93874d926fc500a062011a2aca59bdb9398a45d7
Lucccyo <cha.git@mailo.fr>
2025-06-08 15:21:00 +0200

    add darkmode feature

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import {
   Columns2,
   Eye,
   Pencil,
+  Sun,
+  Moon,
 } from "lucide-react";
 
 import "./index.css";
@@ -30,7 +32,9 @@ export default function App() {
   const [warningUnsaved, setWarningUnsaved] = useState(false);
   const [nextTopicTmp, setNextTopicTmp] = useState<string | null>(null);
   const [deleteTopic, setDeleteTopic] = useState(false);
-
+  const [darkMode, setDarkMode] = useState(() =>
+    document.documentElement.classList.contains("dark")
+  );
   useEffect(() => {
     invoke("list_notes")
       .then((topics) => {
@@ -59,9 +63,15 @@ export default function App() {
     }
   }, [currentTopic]);
 
-  useEffect(() => {
-    marked(note, { async: true }).then(setHtmlPreview);
-  }, [note]);
+useEffect(() => {
+console.log("note =", note);
+  const renderMarkdown = async () => {
+    const html = await marked.parse(note);
+    setHtmlPreview(html);
+    console.log("htmlprev = ", htmlPreview)
+  };
+  renderMarkdown();
+}, [note]);
 
   const refreshTopics = () => {
     invoke("list_notes")
@@ -107,13 +117,19 @@ export default function App() {
     setCurrentTopic(topics[index]);
   };
 
+  const toggleDarkMode = () => {
+    const isDark = !darkMode;
+    setDarkMode(isDark);
+    document.documentElement.classList.toggle("dark", isDark);
+  };
+
   const isEditorVisible = viewMode === "code" || viewMode === "both";
   const isPreviewVisible = viewMode === "preview" || viewMode === "both";
 
   return (
     <div className="flex h-screen">
       <div
-        className={`p-2 border-r text-xs border-r-zinc-200 box-border ${
+        className={`p-2 border-r text-xs border-r-zinc-200 dark:border-r-zinc-700 box-border ${
           menuCollapsed ? "" : "w-[20em]"
         }`}
       >
@@ -141,12 +157,16 @@ export default function App() {
             <Dices size={16} />
             {!menuCollapsed && <span>Random</span>}
           </button>
+         <button onClick={toggleDarkMode} className="cursor-pointer flex items-center gap-2">
+            {darkMode ? <Sun size={16} /> : <Moon size={16} />}
+            {!menuCollapsed && <span>{darkMode ? "Light" : "Dark"} mode</span>}
+          </button>
         </div>
 
         {!menuCollapsed && (
           <div>
-            <h3 className="text-base mt-6">Topics</h3>
-            <ul className="list-none">
+            <h3 className="text-base p-0 m-0 mt-6">Topics</h3>
+            <ul className="list-none p-0 m-0">
               {topics.map((topic) => (
                 <li
                   key={topic}
@@ -159,7 +179,7 @@ export default function App() {
                     }
                   }}
                   className={`text-xs cursor-pointer px-2 py-1 rounded-md ${
-                    topic === currentTopic ? "bg-zinc-200" : "bg-transparent"
+                    topic === currentTopic ? "bg-zinc-200 dark:bg-zinc-700" : "bg-transparent"
                   } truncate`}
                 >
                   {topic}
@@ -171,45 +191,26 @@ export default function App() {
       </div>
 
       <div className="flex p-2 flex-col gap-4 w-screen">
-        <h2 className="text-xl underline">{currentTopic || "No data."}</h2>
+        <h2 className="text-2xl m-0 p-0">{currentTopic || "No data."}</h2>
 
         <div className="flex gap-2 items-center">
-          <div className="flex gap-[2px] rounded-md border border-zinc-200 p-[2px]">
-            <button
-              onClick={() => setViewMode("code")}
-              className={`flex items-center w-[30px] h-[30px] rounded-md justify-center cursor-pointer p-1 ${
-                viewMode === "code" ? "bg-zinc-200" : "bg-white"
-              }`}
-            >
+          <div className="flex gap-[2px] rounded-md border border-zinc-200 dark:border-zinc-700 p-[2px]">
+            <button onClick={() => setViewMode("code")} className={`flex items-center w-[30px] h-[30px] rounded-md justify-center cursor-pointer p-1 ${viewMode === "code" ? "bg-zinc-200 dark:bg-zinc-700" : "bg-white dark:bg-zinc-800"}`}>
               <Pencil size={14} />
             </button>
-
-            <button
-              onClick={() => setViewMode("both")}
-              className={`flex items-center w-[30px] h-[30px] rounded-md justify-center cursor-pointer p-1 ${
-                viewMode === "both" ? "bg-zinc-200" : "bg-white"
-              }`}
-            >
+            <button onClick={() => setViewMode("both")} className={`flex items-center w-[30px] h-[30px] rounded-md justify-center cursor-pointer p-1 ${viewMode === "both" ? "bg-zinc-200 dark:bg-zinc-700" : "bg-white dark:bg-zinc-800"}`}>
               <Columns2 size={14} />
             </button>
-
-            <button
-              onClick={() => setViewMode("preview")}
-              className={`flex items-center w-[30px] h-[30px] rounded-md justify-center cursor-pointer p-1 ${
-                viewMode === "preview" ? "bg-zinc-200" : "bg-white"
-              }`}
-            >
+            <button onClick={() => setViewMode("preview")} className={`flex items-center w-[30px] h-[30px] rounded-md justify-center cursor-pointer p-1 ${viewMode === "preview" ? "bg-zinc-200 dark:bg-zinc-700" : "bg-white dark:bg-zinc-800"}`}>
               <Eye size={14} />
             </button>
           </div>
 
           <button
             onClick={() => setDeleteTopic(true)}
-            className={`flex justify-center items-center rounded-md cursor-pointer w-[36px] h-[36px] ${
-              currentTopic != null
-                ? "bg-red-200"
-                : "bg-zinc-200 text-zinc-500 cursor-not-allowed"
-            }`}
+            className={`flex justify-center items-center rounded-md cursor-pointer w-[36px] h-[36px] 
+${currentTopic != null ? "bg-red-200 dark:bg-red-700" : "bg-zinc-200 dark:bg-zinc-800 text-zinc-500 cursor-not-allowed"}
+            `}
             disabled={!currentTopic}
           >
             <Trash2 size={14} />
@@ -217,11 +218,9 @@ export default function App() {
 
           <button
             onClick={saveNote}
-            className={`flex justify-center items-center rounded-md cursor-pointer w-[36px] h-[36px] ${
-              currentTopic != null && isModified
-                ? "bg-green-200"
-                : "bg-zinc-200 text-zinc-500 cursor-not-allowed"
-            }`}
+            className={`flex justify-center items-center rounded-md cursor-pointer w-[36px] h-[36px]
+${currentTopic != null && isModified ? "bg-green-200 dark:bg-green-700" : "bg-zinc-200 dark:bg-zinc-800 text-zinc-500 cursor-not-allowed"}
+            `}
             disabled={!currentTopic}
           >
             <Save size={14} />
@@ -230,9 +229,9 @@ export default function App() {
 
         {isEditorVisible && (
           <>
-            <h3>Editor</h3>
+            <h3 className="text-base p-0 m-0">Editor</h3>
             <textarea
-              className={`p-2 rounded-md border resize-none outline-none text-xs flex font-mono ${
+              className={`mt-[-10px] p-2 rounded-md border resize-y outline-none text-xs flex font-mono bg-white dark:bg-zinc-800 border-zinc-200 dark:border-zinc-700 text-black dark:text-white ${
                 isPreviewVisible ? "h-[50%]" : "h-[100%]"
               }`}
               value={note}
@@ -244,37 +243,40 @@ export default function App() {
 
         {isPreviewVisible && (
           <>
-            <h3>Preview</h3>
-            <div
-              className={`p-2 rounded-md border resize-none outline-none text-xs flex font-mono ${
-                isEditorVisible ? "h-[50%]" : "h-[100%]"
-              }`}
-              dangerouslySetInnerHTML={{ __html: htmlPreview }}
-            />
+            <h3 className="text-base p-0 m-0">Preview</h3>
+<div
+  className={`mt-[-10px] p-2 rounded-md border outline-none text-xs flex flex-col w-full
+    bg-white dark:bg-zinc-800 border-zinc-200 dark:border-zinc-700 text-black dark:text-white preview-html
+    overflow-y-auto overflow-x-hidden break-words whitespace-pre-wrap
+    ${isEditorVisible ? "h-[50%]" : "h-[100%]"}`}
+  dangerouslySetInnerHTML={{ __html: htmlPreview }}
+/>
           </>
         )}
       </div>
 
       {showNewTopicModal && (
-        <div className="fixed inset-0 bg-zinc-500 bg-opacity-50 flex justify-center items-center z-999">
-          <div className="flex flex-col gap-2 bg-white p-4 rounded-md shadow-md">
-            <h3>New Topic</h3>
+        <div className="fixed inset-0 bg-zinc-900 bg-opacity-50 flex justify-center items-center z-999">
+          <div className="flex flex-col gap-2 bg-white dark:bg-zinc-800 p-4 rounded-md shadow-md">
+            <h3 className="m-0 p-0">New Topic</h3>
             <input
               type="text"
               value={newTopicInput}
               onChange={(e) => setNewTopicInput(e.target.value)}
-              className="p-2 text-xs border rounded-md border-zinc-200"
+              className="p-2 text-xs border rounded-md bborder-zinc-200 dark:border-zinc-700 bg-white dark:bg-zinc-800"
               placeholder="Enter topic name"
             />
-            {error && <div className="text-red-500">{error}</div>}
             <div className="flex justify-end gap-2">
               <button
-                className="text-xs"
+                className="flex cursor-pointer gap-2 items-center justify-center p-2 bg-zinc-200 dark:bg-zinc-700 rounded-md text-xs"
                 onClick={() => setShowNewTopicModal(false)}
               >
                 Cancel
               </button>
-              <button className="text-xs" onClick={confirmAddTopic}>
+              <button
+                className="flex cursor-pointer gap-2 items-center justify-center p-2 bg-green-200 dark:bg-green-700 rounded-md text-xs"
+                onClick={confirmAddTopic}
+              >
                 Add
               </button>
             </div>
@@ -283,22 +285,22 @@ export default function App() {
       )}
 
       {warningUnsaved && (
-        <div className="fixed inset-0 bg-zinc-500 bg-opacity-50 flex justify-center items-center z-999">
-          <div className="flex flex-col gap-2 bg-white p-4 rounded-md shadow-md">
-            <h3>Warning</h3>
+        <div className="fixed inset-0 bg-zinc-900 bg-opacity-50 flex justify-center items-center z-999">
+          <div className="flex flex-col gap-2 bg-white dark:bg-zinc-800 p-4 rounded-md shadow-md">
+            <h3 className="m-0 p-0">Warning</h3>
             <div className="flex flex-col text-xs">
               <span>Your current note has been modified but not saved.</span>
               <span>If you switch topics, your changes will be lost.</span>
             </div>
             <div className="flex justify-end gap-2">
               <button
-                className="flex cursor-pointer gap-2 items-center justify-center p-2 bg-zinc-200 rounded-md text-xs"
+                className="flex cursor-pointer gap-2 items-center justify-center p-2 bg-zinc-200 dark:bg-zinc-700 rounded-md text-xs"
                 onClick={() => setWarningUnsaved(false)}
               >
                 Cancel
               </button>
               <button
-                className="flex cursor-pointer gap-2 items-center justify-center p-2 bg-red-200 rounded-md text-xs"
+                className="flex cursor-pointer gap-2 items-center justify-center p-2 bg-red-200 dark:bg-red-700 rounded-md text-xs"
                 onClick={() => {
                   if (nextTopicTmp) setCurrentTopic(nextTopicTmp);
                   setWarningUnsaved(false);
@@ -313,22 +315,22 @@ export default function App() {
       )}
 
       {deleteTopic && (
-        <div className="fixed inset-0 bg-zinc-500 bg-opacity-50 flex justify-center items-center z-999">
-          <div className="flex flex-col gap-2 bg-white p-4 rounded-md shadow-md">
-            <h3>Delete Topic</h3>
+        <div className="fixed inset-0 bg-zinc-900 bg-opacity-50 flex justify-center items-center z-999">
+          <div className="flex flex-col gap-2 bg-white dark:bg-zinc-800 p-4 rounded-md shadow-md">
+            <h3 className="m-0 p-0">Delete Topic</h3>
             <div className="flex flex-col text-xs">
               <span>Are you sure you want to delete this topic?</span>
               <span>This action cannot be undone.</span>
             </div>
             <div className="flex justify-end gap-2">
               <button
-                className="flex cursor-pointer gap-2 items-center justify-center p-2 bg-zinc-200 rounded-md text-xs"
+                className="flex cursor-pointer gap-2 items-center justify-center p-2 bg-zinc-200 dark:bg-zinc-700 rounded-md text-xs"
                 onClick={() => setDeleteTopic(false)}
               >
                 Cancel
               </button>
               <button
-                className="flex cursor-pointer gap-2 items-center justify-center p-2 bg-red-200 rounded-md text-xs"
+                className="flex cursor-pointer gap-2 items-center justify-center p-2 bg-red-200 dark:bg-red-700 rounded-md text-xs"
                 onClick={() => {
                   if (!currentTopic) return;
                   invoke("delete_note", { word: currentTopic })
